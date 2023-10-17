84646081c18592662735fc1405a9547ab2f21f64
Muhammad Afiq Samsudin <ahhhfiq@Olympus.local>
2023-10-17 16:49:43 +0800

    Added Docker

--- web/app.js
+++ web/app.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const apiV1Router = require('./api/routes');
-const query = require('./services/dbService');
+// const apiV1Router = require('./api/routes');
+// const query = require('./services/dbService');
 const config = require('./config/config');
 const cookieParser = require('cookie-parser');
 
@@ -16,22 +16,27 @@ app.use(
 
 // let port = process.env.PORT || 8080;
 // let host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 8080;
-const host = process.env.HOST || '0.0.0.0';
+const port = config.port || 8000;
+const host = config.host || '0.0.0.0';
 
-const pageRouter = express.Router();
+// const pageRouter = express.Router();
 // app.use(helmet());
 // app.use(express.static(path.join(__dirname, 'views')));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.set('views', path.join(__dirname, 'views'));
 app.use('/public', express.static('public'));
 
-
-
 // // Global Error Handler
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send('Something went wrong!');
-// });
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 app.listen(port, () => console.log(`Server running on ${host}:${port}`));
---
+++ web/config/config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+config = {
+    devOrProd: process.env.NODE_ENV,
+    uri: process.env.MYSQL_URI,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASSWORD,
+    db_name: process.env.DB_NAME,
+    db_port: process.env.DB_PORT,
+    db_dialect: process.env.DB_DIALECT,
+    db_host: process.env.DB_HOST,
+    passSecret: process.env.passSecret,
+    passSalt: process.env.passSalt,
+    port: process.env.PORT,
+    host: process.env.HOST
+};
+
+module.exports = config;
