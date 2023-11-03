87934012210b3e011f233596df1d633e1655476a
ahhhfiq <ahhhfiq@gmail.com>
2023-11-03 17:34:30 +0800

    Created Routers

--- web/api/camera/cameraRouter.js
+++ web/api/camera/cameraRouter.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const { authToken } = require('../../services/authentication');
 const cameraRouter = express.Router();
 
+cameraRouter.get('/', authToken);
+cameraRouter.get('/:id', authToken);
+cameraRouter.post('/new-camera', authToken);
 
 
 module.exports = cameraRouter;
--- web/api/reports/reportRouter.js
+++ web/api/reports/reportRouter.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const { authToken } = require('../../services/authentication');
 const reportRouter = express.Router();
 
-
+reportRouter.get('/', authToken);
+reportRouter.get('/:id', authToken);
+reportRouter.post('/submit', authToken);
+reportRouter.put('/:id/accept', authToken);
+reportRouter.put('/:id/reject', authToken);
+reportRouter.delete('/:id', authToken);
 
 module.exports = reportRouter;
