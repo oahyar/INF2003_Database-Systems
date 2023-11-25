98e65aca7821584a61c672922d3fc44ac1b2bd15
ahhhfiq <ahhhfiq@gmail.com>
2023-11-26 02:27:19 +0800

    Quick fix

--- web/tools.sh
+++ web/tools.sh
@@ -6,36 +6,57 @@ container_name=web
 dev=1
 prod=2
 ssl=3
+
 user_choice() {
-    read -p "Choose: 
+    while true; do
+        read -p "Choose: 
 1) Development
 2) Production
 3) Renew SSL
 " choice
-echo ""
+        echo ""
+        case $choice in
+            $dev)
+                echo "Setting up Development environment..."
+                break
+                ;;
+            $prod)
+                echo "Setting up Production environment..."
+                break
+                ;;
+            $ssl)
+                echo "Renewing SSL..."
+                break
+                ;;
+            *)
+                echo "Invalid option, please try again."
+                ;;
+        esac
+    done
 }
+
 user_choice
 
 if [[ $choice -eq $dev ]]
 then
-    docker build -t $image_name -f Dockerfile  .
+    docker build -t $image_name -f Dockerfile .
 
-    echo Deleting old container...
+    echo "Deleting old container..."
     docker rm -f $container_name
 
-    echo Creating new container...
+    echo "Creating new container..."
     docker run -d -p $port_id':'$port_id --name $container_name $image_name
 elif [[ $choice -eq $prod ]]
 then
-    #This script restarts the docker compose
-    echo Stopping all containers
+    # This script restarts the docker compose
+    echo "Stopping all containers"
     docker-compose stop
 
-    echo Removing all containers
+    echo "Removing all containers"
     docker-compose rm -f
 
-    echo Starting containers...
-    docker-compose up -f docker-compose.prod.yml --build -d
+    echo "Starting containers..."
+    docker-compose up --build -d
 elif [[ $choice -eq $ssl ]]
 then
     COMPOSE="/usr/bin/docker-compose --no-ansi"
@@ -43,8 +64,4 @@ then
 
     $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
     $DOCKER system prune -af
-else
-    echo "Option unavailable, please try again"
-
-    user_choice
 fi
